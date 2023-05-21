11baf087c12d8bf42a57abefec1814389e7cc8f5
itasumit <itasumit@gmail.com>
2023-05-21 15:26:15 +0530

    enable cors

--- Capstone.Gateway/Program.cs
+++ Capstone.Gateway/Program.cs
@@ -3,12 +3,13 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Configuration.AddJsonFile("ocelot.json",optional:false,reloadOnChange:true);
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 builder.Services.AddOcelot(builder.Configuration);
 
 var app = builder.Build();
 
 //app.MapGet("/", () => "Hello World!");
 
+app.UseCors(o => o.AllowAnyHeader().AllowAnyOrigin().AllowAnyHeader());
 app.UseOcelot();
 app.Run();
